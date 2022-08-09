9d75439d783b2a822f8aa0891d89727e671f5b0b
yashwanth lal <yashwanth.lal@zemosolabs.com>
2022-08-09 20:32:31 +0530

    added assi-1

---
+++ assignmentt-1/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { MainIndex } from './mainIndex';
+
+function App() {
+  return (
+    <div>
+      <MainIndex />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ assignmentt-1/src/constants.ts
@@ -0,0 +1,5 @@
+export const maleIcon = "https://flyclipart.com/thumb2/male-user-icon-19810.png";
+export const femaleIcon = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWyyfxx1WBvzXZoXn5RddyQpADV34Dz90sf2PzHIVHbFK2o73xVGz49jiYqS8k-LvRRag&usqp=CAU";
+export const kidImage = "https://images.ctfassets.net/cnu0m8re1exe/PYLeBWr2QDF5yjyZwB5Iq/7509e49d1d621860c1d4c484243c23db/shutterstock_189429203.jpg"
+export const boyImage= "https://cdn2.storyasset.link/TTIy4aLxXLZ52bis4RuaasCifjW2/3-ms-zibehchcdh.jpg"
+export const manImage="https://static.theceomagazine.net/wp-content/uploads/2018/10/15093202/elon-musk.jpg"
